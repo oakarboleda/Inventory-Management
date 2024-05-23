40d2e3ffc0b5f8197559ac54c211fbd292fda9a3
Oak Arboleda <oak.arboleda@sharedcreatives.com>
2024-05-23 09:34:54 -0500

    Decided to use Supbase instead.

--- server/src/routes/item.ts
+++
@@ -1,9 +0,0 @@
-export {}; // Needed otherwise transpiler will complain about 'cannot redeclare variable'
-const express = require("express");
-const router = express.Router();
-// const { checkAuthenticated } = require('../middleware/authentication');
-const itemController = require("../controllers/item");
-
-router.get("/get", itemController.get);
-
-module.exports = router;
--- server/src/routes/profile.ts
+++
@@ -1,9 +0,0 @@
-export {}; // Needed otherwise transpiler will complain about 'cannot redeclare variable'
-const express = require("express");
-const router = express.Router();
-const { checkAuthenticated } = require("../middleware/authentication");
-const profileController = require("../controllers/profile");
-
-router.get("/get", checkAuthenticated, profileController.get);
-
-module.exports = router;
